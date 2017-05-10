175d37840bd6413e7aad9499af23fccaec9be6d1
krystiank907 <krystian.k907@gmail.com>
2017-05-10 11:03:32 +0200

    opuszczanie sie podlogi oraz restart

--- game_Gecko_Lab/GameScene.swift
+++ game_Gecko_Lab/GameScene.swift
@@ -15,30 +15,33 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var step:SKSpriteNode?
     var left:SKSpriteNode?
     var right:SKSpriteNode?
+    var floor:SKSpriteNode?
     var info = true
+    var first = true
+    var lose:SKLabelNode?
     
     
     
     override func didMove(to view: SKView) {
         
+        //add background
         let background = SKSpriteNode(imageNamed: "b1")
         self.addChild(background)
         background.size = CGSize(width: 750, height: 1334)//set width and height background and possiton x,y,z
         background.position=CGPoint(x: self.frame.midX, y: self.frame.midY)
         background.zPosition = 0
         
-        let floorImage = SKSpriteNode(imageNamed: "b4")
-        floorImage.size = CGSize(width: 750, height: 640)
-        floorImage.position=CGPoint(x: self.frame.midX, y: -346)
-        floorImage.zPosition = 0.001
-        self.addChild(floorImage)
+        setupFloor()//add floor
         
+        //add button move to left
         left = SKSpriteNode()
         left?.size = CGSize(width: 350, height: 350)
         left?.anchorPoint = CGPoint(x: 0, y: 0)
         left?.zPosition = 0.01
         left?.position = CGPoint(x: self.frame.minX, y: self.frame.minY)
         self.addChild(left!)
+        
+        //button move to right
         right = SKSpriteNode()
         right?.size = CGSize(width: 350, height: 350)
         right?.anchorPoint = CGPoint(x: 1, y: 0)
@@ -46,87 +49,117 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         right?.position = CGPoint(x: self.frame.maxX, y: self.frame.minY)
         self.addChild(right!)
         
-        let floorPhysics = SKNode()
-        floorPhysics.physicsBody = SKPhysicsBody(edgeFrom: CGPoint(x: self.frame.minX, y: -504), to: CGPoint(x: self.frame.maxX, y: -504))
-        self.addChild(floorPhysics)
-        
+        //add left Physics margines of scene
         let leftMarginesPhysics = SKNode()
         leftMarginesPhysics.physicsBody = SKPhysicsBody(edgeFrom: CGPoint(x: self.frame.minX , y: self.frame.minY), to: CGPoint(x: self.frame.minX, y: self.frame.maxY))
         self.addChild(leftMarginesPhysics)
         
+        //add right Physics margines of scene
         let rigtMarginesPhysics = SKNode ()
         rigtMarginesPhysics.physicsBody = SKPhysicsBody(edgeFrom: CGPoint(x: self.frame.maxX, y: self.frame.minY), to: CGPoint(x: self.frame.maxX, y: self.frame.maxY))
         self.addChild(rigtMarginesPhysics)
         
+        //install monkey on start position
+        setupMonkey()
         
-        monkey = SKSpriteNode(imageNamed: "left_2")
-        monkey?.position = CGPoint(x: 305, y: -370)
-        monkey?.zPosition = 0.01
-        self.addChild(monkey!)
-        monkey?.physicsBody = SKPhysicsBody(circleOfRadius: 30)
+        //setup gravity of game world
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: -5)
         
-        self.physicsWorld.gravity = CGVector(dx: 0, dy: -10)
-        
+        //setup bitmask monkey
         monkey?.physicsBody?.categoryBitMask = 2
-        
         monkey?.physicsBody?.contactTestBitMask = 1
-        
         monkey?.physicsBody?.collisionBitMask = 1
-        monkey?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-        //self.physicsWorld.contactDelegate = self
         
+        setupStep()//add step
         
-        setupStep()
+        //add loseLabel
+        lose = SKLabelNode(fontNamed: "Helvetica Neue Light Italic")
+        lose?.text = "Lose"
+        lose?.fontSize = 90
+        lose?.zPosition = 0.01
+        lose?.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        lose?.run(pulse())
+        self.addChild(lose!)
+        
+    }
+    
+    func setupMonkey(){
+        monkey = SKSpriteNode(imageNamed: "left_2")
+        monkey?.position = CGPoint(x: 305, y: -170)
+        monkey?.zPosition = 0.01
+        monkey?.physicsBody?.allowsRotation = false
+        monkey?.physicsBody = SKPhysicsBody(circleOfRadius: 30)
+        self.addChild(monkey!)
+    }
+    
+    func setupFloor(){
+        floor = SKSpriteNode(imageNamed: "b5")
+        floor?.size = CGSize(width: 750, height: 350)
+        floor?.anchorPoint = CGPoint(x: 0.5, y: 0.60)
+        floor?.position=CGPoint(x: self.frame.midX, y: self.frame.minY)
+        floor?.zPosition = 0.001
+        self.addChild(floor!)
+        floor?.physicsBody = SKPhysicsBody(texture: floor!.texture!, size: floor!.size)
+        floor?.physicsBody?.isDynamic = false
+        floor?.physicsBody?.categoryBitMask = 1
+        floor?.physicsBody?.contactTestBitMask = 2
+        floor?.physicsBody?.collisionBitMask = 2
+        floor?.run(SKAction.moveBy(x: 0, y: -300 , duration: 15.5))
         
     }
     
     func setupStep(){
+        let minX = self.frame.minX
+        let maxX = self.frame.maxX
+        let minY = self.frame.minY
+        let maxY = self.frame.maxY
+        let resultX = CGFloat(arc4random_uniform(UInt32(maxX - minX + 1))) + minX
+        let resultY = CGFloat(arc4random_uniform(UInt32(maxY - minY + 1))) + minY
         step = SKSpriteNode(imageNamed: "b5")
         step?.size = CGSize(width: 350, height: 60)
-        step?.position = CGPoint(x: 305, y: -270)
+        step?.position = CGPoint(x: resultX, y: resultY)
         step?.anchorPoint = CGPoint(x: 0.5, y: 1.25)
         step?.zPosition = 0.001
         self.addChild(step!)
         step?.physicsBody = SKPhysicsBody(texture: step!.texture!, size: step!.size)
         step?.physicsBody?.isDynamic = false
         
-        
-    }
-    func didBegin(_ contact: SKPhysicsContact) {
-        self.physicsWorld.speed = 0.1
-        step?.removeAllActions()
     }
     
-    
-    override func update(_ currentTime: TimeInterval) {
-        /* Called before each frame is rendered */
-    }
-    
+    //single touch
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
             let location = touch.location(in: self)
             let touchedNode = self.atPoint(location)
             if touchedNode == monkey {
-                monkey?.run(SKAction.moveBy(x: 0, y: 50, duration: 0.1))
+                monkey?.run(SKAction.moveBy(x: 0, y: 150, duration: 0.1))
                 monkey?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
                 possitionStep()
+                left?.isUserInteractionEnabled = true
+                right?.isUserInteractionEnabled = true
             }
             if touchedNode == left {
                 monkey?.run(SKAction.group([actionMonkeyLeft(),wait(),moveLeft()]))
                 monkey?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
                 possitionStep()
+                right?.isUserInteractionEnabled = true
+                monkey?.isUserInteractionEnabled = true
                 
             }
             if touchedNode == right {
                 monkey?.run(SKAction.group([actionMonkeyRight(),wait(),moveRight()]))
                 monkey?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
                 possitionStep()
+                left?.isUserInteractionEnabled = true
+                monkey?.isUserInteractionEnabled = true
                 
             }
             
             
         }
     }
+    
+    //hold touch
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         for touch in touches {
@@ -138,9 +171,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                     monkey?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
                     possitionStep()
                     self.info = false
+                    monkey?.isUserInteractionEnabled = true
+                    right?.isUserInteractionEnabled = true
                 }else{
                     monkey?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
                     possitionStep()
+                    monkey?.isUserInteractionEnabled = true
+                    right?.isUserInteractionEnabled = true
                 }
             }
             if touchedNode == right {
@@ -149,27 +186,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                     monkey?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
                     possitionStep()
                     self.info = false
+                    left?.isUserInteractionEnabled = true
+                    monkey?.isUserInteractionEnabled = true
                 }else{
                     monkey?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
                     possitionStep()
+                    left?.isUserInteractionEnabled = true
+                    monkey?.isUserInteractionEnabled = true
                 }
-            }
-            if touchedNode == monkey {
-                monkey?.run(SKAction.moveBy(x: 0, y: 50, duration: 0.1))
-                monkey?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-                possitionStep()
             }
             
         }
         
     }
+    
+    //finish touches
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.info=true
         monkey?.removeAllActions()
-        monkey?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
-        
+        monkey?.physicsBody?.allowsRotation = false
+        turnOnInteraction()
     }
     
+    //animation for left move monkey
     func actionMonkeyLeft()->SKAction{
         let action = SKAction.repeatForever(
             SKAction.animate(with: [
@@ -180,6 +219,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         return action
     }
     
+    //animation for right move monkey
     func actionMonkeyRight()->SKAction{
         let action = SKAction.repeatForever(
             SKAction.animate(with: [
@@ -190,20 +230,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         return action
     }
     
+    //move monkey left
     func moveLeft()->SKAction{
-        let moveNodeUp = SKAction.repeatForever(SKAction.moveBy(x: -20.0, y: 0.0, duration: 0.1))
+        let moveNodeUp = SKAction.repeatForever(SKAction.moveBy(x: -20.0, y: -5.0, duration: 0.1))
         return moveNodeUp
     }
     
+    //move monkey right
     func moveRight()->SKAction{
-        let moveNodeUp = SKAction.repeatForever(SKAction.moveBy(x: 20.0, y: 0.0, duration: 0.1))
+        let moveNodeUp = SKAction.repeatForever(SKAction.moveBy(x: 20.0, y: -5.0, duration: 0.1))
         return moveNodeUp
     }
     
+    //wait
     func wait()->SKAction{
         let wait = SKAction.wait(forDuration: 0.1)
         return wait
     }
+    
+    //set BitMask for step
     func possitionStep(){
         let position = self.convert((step?.position)!, to: self)
         let position2 = self.convert((monkey?.position)!, to: self)
@@ -217,6 +262,56 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             step?.physicsBody?.collisionBitMask = 2
         }
         
+    }
+    
+    //turn on interaction for monkey and button
+    func turnOnInteraction(){
+        monkey?.isUserInteractionEnabled = false
+        right?.isUserInteractionEnabled = false
+        left?.isUserInteractionEnabled = false
+        
+    }
+    
+    //pulse animation for LoseLabel
+    func pulse()->SKAction{
+        let pulseUp = SKAction.scale(to: 1.5, duration: 0.5)
+        let pulseDown = SKAction.scale(to: 1.0, duration: 0.5)
+        let pulse = SKAction.sequence([pulseUp, pulseDown])
+        let repeatPulse = SKAction.repeatForever(pulse)
+        return repeatPulse
+    }
+    
+    func restart(){
+        floor?.removeFromParent()
+        step?.removeFromParent()
+        monkey?.removeFromParent()
+        setupStep()
+        setupFloor()
+        setupMonkey()
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        self.physicsWorld.speed = 0.0
+        step?.removeAllActions()
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        let position = self.convert((monkey?.position)!, to: self)
+        print(position.y)
+        monkey?.physicsBody?.allowsRotation = false
+        if position.y > -730 && first == false{
+            lose?.run(SKAction.fadeOut(withDuration: 1.0))
+        }
+        if position.y > -730 && first == true{
+            lose?.run(SKAction.fadeOut(withDuration: 0.01))
+            first = false
+        }
+        if position.y < -735{
+            lose?.run(SKAction.fadeIn(withDuration: 0.01))
+            restart()
+            
+        }
     }
     
     
